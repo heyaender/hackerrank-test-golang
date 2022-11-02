20a5b971921837ec881ba41bff58abb08d4cad01
Andrianto Cahyono <andri.c.putro@gmail.com>
2022-11-02 16:05:08 +0700

    Update main.go

--- day07/main.go
+++ day07/main.go
@@ -11,14 +11,26 @@ import (
 var arraySize int
 
 func main() {
+
+	// Taking the input from the user and storing it in the variable arraySize.
 	fmt.Scan(&arraySize)
+
+	// Creating a slice of type int with the size of arraySize.
 	array := make([]int, arraySize)
+
+	// Creating a new scanner object which will read from the standard input.
 	arrayElements := bufio.NewScanner(os.Stdin)
+	// Scanning the input from the standard input.
 	arrayElements.Scan()
+
+	// Storing the input from the standard input in the variable data.
 	data := arrayElements.Text()
+	// Splitting the string data into a slice of strings.
 	newData := strings.Split(data, " ")
 
+	// Storing the length of the array in the variable size.
 	size := len(array)
+
 	for _, k := range newData {
 		array[size-1], _ = strconv.Atoi(k)
 		size--
