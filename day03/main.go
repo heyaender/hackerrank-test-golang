9f24f644a496578981f78ec37b36d2fad5aa2d7d
Andrianto Cahyono <andri.c.putro@gmail.com>
2022-11-02 10:42:12 +0700

    day3

---
+++ day03/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	NTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	N := int32(NTemp)
+
+	fmt.Scan(&N)
+
+	if N%2 != 0 {
+		fmt.Println("Weird")
+	}
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
